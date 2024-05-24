f1ebf5ac652947f8f59f554c07991eeedec836c8
Alexer <41583014+AlexerCrafter@users.noreply.github.com>
2024-05-24 08:50:44 +0200

    Update SauvPoids.py

--- Alexis/SauvPoids.py
+++ Alexis/SauvPoids.py
@@ -1,1 +1,22 @@
+import mysql.connector
+import datetime
 
+# Création de la fonction qui nous permet de se connecter à la base de données et d'effectuer des actions
+def connexion():
+  db = mysql.connector.connect(
+    host = "localhost",
+    user = "root@localhost",
+    password = "",
+    database = "Soin_Animal")
+  cursor = db.cursor()
+
+  # On insert le poids qui a été pesé
+  cursor.execute(""" INSERT INTO 'Soin_Animal.Suivi.Poids_Pese' VALUES (?); """)
+  de.commit()
+
+  # On récupère la Date, le Poids pesé et le Poids actuel
+  cursor.execute(""" SELECT Suivi.Date, Suivi.Poids_Pese, Suivi.Poids_Actu FROM Soin_Animal.Animal; """)
+  db.commit()
+
+  # On ferme la base de données
+  db.close()
